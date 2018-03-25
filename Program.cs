7ed15fb804b97f6e848c98c85a76dbae47b5f964
SpaceApples <7674856@students.swin.edu.au>
2018-03-25 15:02:45 +1100

    Parsing text
    Parsing inner text via xpath and outputing it

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HtmlAgilityPack;
+
+namespace HtmlParserTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String url = "https://www.swinburne.edu.au/study/courses/units/Object-Oriented-Programming-COS20007/local";
+            HtmlWeb web = new HtmlWeb();
+            HtmlDocument doc = web.Load(url);
+
+            string prerequisite = doc.DocumentNode.SelectSingleNode("//*[@id='content']/main/section[1]/div/div[3]/div[2]/a[1]").InnerText;
+
+            Console.WriteLine(prerequisite);
+        }
+    }
+}
